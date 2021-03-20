a1a8628fcb8cbba429ffa8db54671ca977ce916c
Mikhail Uljakov <pugumo68@gmail.com>
2021-03-21 03:39:49 +0700

    add min window size.

--- src/background.js
+++ src/background.js
@@ -16,8 +16,9 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    minWidth:800,
+    minHeight: 600,
     webPreferences: {
-      
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
