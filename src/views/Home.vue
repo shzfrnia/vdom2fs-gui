fa62cb9697145ca8be36b51398d3a89b489844c0
Mikhail Uljakov <pugumo68@gmail.com>
2021-03-30 00:12:10 +0700

    fix home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="home">
     <default>Hello </default>
-  </div>
 </template>
 
 <script>
-import Default from "../layouts/Default"
+import Default from "../layouts/Default";
 
 export default {
   components: {
-    Default
+    Default,
   },
   name: "Home",
 };
