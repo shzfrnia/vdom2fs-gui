4c1e74b1c79ae9d70c3080bb600faf49f4f7bad7
Mikhail Uljakov <pugumo68@gmail.com>
2021-04-04 06:47:37 +0700

    add export app instance

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import store from "./store/index";
 import ElementPlus from "element-plus";
 import "element-plus/lib/theme-chalk/index.css";
 
-createApp(App)
+const app = createApp(App)
   .use(router)
   .use(store)
   .use(ElementPlus)
   .mount("#app");
+
+export default app;
