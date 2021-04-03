24958e93f0bc269f0e8fb12bf3ef620ed597d4ae
Mikhail Uljakov <pugumo68@gmail.com>
2021-04-04 06:47:12 +0700

    add loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <el-table
+    v-loading="loading"
+    element-loading-text="Loading..."
+    element-loading-spinner="el-icon-loading"
+    element-loading-background="rgba(0, 0, 0, 0.8)"
+    :data="tableData"
+    style="width: 100%"
+  />
+</template>
+
+<script>
+export default {
+  props: [],
+};
+</script>
+
+<style>
+</style>
