d8943cad5859363296d72eec2c7f139501b240bd
Mikhail Uljakov <pugumo68@gmail.com>
2021-04-04 06:45:35 +0700

    add check path on app create

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,32 @@
   <router-view />
 </template>
 
+<script>
+import { mapActions, mapGetters } from "vuex";
+
+export default {
+  methods: {
+    ...mapActions("vdom2fs", ["checkVdom2fsFolderOnValid"]),
+  },
+  computed: {
+    ...mapGetters("vdom2fs", ["currentPath"]),
+  },
+  async created() {
+    this.$store.commit("setLoading", true);
+    try {
+      await this.checkVdom2fsFolderOnValid(this.currentPath);
+    } catch (error) {
+      this.$router.router.push("Setup");
+    } finally {
+      this.$store.commit("setLoading", false);
+    }
+  },
+};
+</script>
 <style>
 * {
-  font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+  font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB",
+    "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
