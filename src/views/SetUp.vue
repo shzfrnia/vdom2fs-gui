620b665f7516efa664d11116ceae7c22c8b57e92
Mikhail Uljakov <pugumo68@gmail.com>
2021-04-04 06:50:10 +0700

    develop setup page

--- src/views/SetUp.vue
+++ src/views/SetUp.vue
@@ -4,7 +4,6 @@
       <el-button @click="chooseFolder" type="primary">
         Настроить папку
       </el-button>
-      <div>{{pathErrors}}</div>
     </el-empty>
   </header-and-footer>
 </template>
@@ -12,21 +11,24 @@
 <script>
 import HeaderAndFooter from "@/layouts/HeaderAndFooter";
 import { remote } from "electron";
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 
 export default {
   components: {
     HeaderAndFooter,
   },
   computed: {
-    ...mapGetters('vdom2fs', [
-      'pathErrors'
-    ])
+    ...mapGetters("vdom2fs", ["pathErrors"]),
+  },
+  watch: {
+    pathErrors() {
+      this.printPathErrors();
+    },
   },
   methods: {
-    ...mapActions('vdom2fs', [
-      'checkVdom2fsFolderOnValid'
-    ]),
+    ...mapActions("vdom2fs", ["checkVdom2fsFolderOnValid"]),
+    ...mapActions(["notify"]),
+    ...mapActions("notification", ["clearAll"]),
     chooseFolder() {
       remote.dialog
         .showOpenDialog({ properties: ["openDirectory"] })
@@ -36,9 +38,23 @@ export default {
           }
         })
         .catch((err) => {
-          console.log(err);
+          console.error(err);
         });
     },
+    async printPathErrors() {
+      this.clearAll();
+      this.pathErrors.forEach(async (el) => {
+        this.notify({
+          title: el.file,
+          message: el.message,
+          duration: 0,
+          type: "error",
+        });
+      });
+    },
+  },
+  created() {
+    this.printPathErrors();
   },
 };
 </script>
