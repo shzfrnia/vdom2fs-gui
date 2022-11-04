973941bc0fad3de32d896249dde32002c6b8c291
Mikhail Uljakov <pugumo68@gmail.com>
2022-11-04 22:35:27 +0700

    fix max os build add icon

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
+const fs = require('fs')
+const packageJson = fs.readFileSync('./package.json')
+const version = JSON.parse(packageJson).version || 0
 const webpack = require('webpack')
 
 module.exports = {
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
-        'process.env': {},
+        'process.env': { PACKAGE_VERSION: `"${version}"` },
       }),
     ],
   },
